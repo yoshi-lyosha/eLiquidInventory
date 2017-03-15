f9c2f3ba01609e47a9f09d069492a9724006ef63
eremeevfd <dartfedor@mail.ru>
2017-03-15 12:39:13 +0300

    Adds users and flavors.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from eLiquid import ELiquid
+from user import User
 
 
 if __name__ == '__main__':
+    user1 = User('Fedos')
+    user2 = User('Koresh')
+    user3 = User('Olga')
     dragonblood = ELiquid("Dragon blood")
     print(dragonblood.name)
     dragonblood.set_ingredients(['sweet', 'shit', 'dragon', 'strawberry'])
     print(dragonblood.ingredients)
     print(dragonblood)
+    user1.add_flavor('strawberry')
+    print(user1.name, user1.flavors)
--- user.py
+++ user.py
@@ -2,7 +2,10 @@ class User:
 
     def __init__(self, name):
         self.name = name
+        self.flavors = []
 
     def show_available_liquid_recipes(self):
         pass
 
+    def add_flavor(self, flavor_name):
+        self.flavors.append(flavor_name)
