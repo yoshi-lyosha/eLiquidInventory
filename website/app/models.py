0248fc17d2c11a16b7a27d6520f3bacc067918e6
yoshi-lyosha <atanov_wow@mail.ru>
2017-04-02 22:19:19 +0300

    Добавил в модель базовые классы, необходимо добавить остальные, наследующие их. Небольшое косметическое изменение структуры бд

--- website/app/models.py
+++ website/app/models.py
@@ -1,19 +1,53 @@
 from website.app import db
 from website.app.users import constants as USER
-
-ROLE_USER = 0
-ROLE_ADMIN = 1
+from website.app.eliquids import constants as ELIQUID
 
 
 class User(db.Model):
 
-    __tablename__ = 'totally_not_users_db'
+    __tablename__ = 'Users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True)
+    user_name = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
     role = db.Column(db.SmallInteger, default=USER.USER)
     status = db.Column(db.SmallInteger, default=USER.NEW)
+    # нужно ли это?
+    eLiquids = db.relationship('ELiquid', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+
+class Nicotine(db.Model):
+
+    __tablename__ = 'Nicotine'
+    id = db.Column(db.Integer, primary_key=True)
+    producer_name = db.Column(db.String(64))
+    concentration = db.Column(db.SmallInteger)
+
+    def __repr__(self):
+        return '<Nicotine %r>' % (self.producer_name)
+
+
+class Flavor(db.Model):
+
+    __tablename__ = 'Flavors'
+    id = db.Column(db.Integer, primary_key=True)
+    flavor_name = db.Column(db.String(64))
+    producer_name = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Flavor %r>' % (self.flavor_name) % (self.producer_name)
+
+
+class ELiquid(db.Model):
+
+    __tablename__ = 'eLiquids'
+    id = db.Column(db.Integer, primary_key=True)
+    eliquid_name = db.Column(db.String(120), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    status = db.Column(db.SmallInteger, default=ELIQUID.PUBLIC)
+
+    def __repr__(self):
+        return '<Flavor %r>' % (self.eliquid_name)
