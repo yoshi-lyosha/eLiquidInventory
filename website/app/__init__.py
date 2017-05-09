dc7befcede47485adc6966a0fb5b0f4675003e64
eremeevfd <dartfedor@mail.ru>
2017-05-09 13:50:07 +0300

    flask-migrations

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config.from_object('website.config')
+app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # lm = LoginManager()
 # lm.init_app(app)
 # lm.login_view = 'login'
 
-from website.app import views, models
+from app import views, models
