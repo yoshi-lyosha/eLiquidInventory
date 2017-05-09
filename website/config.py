8597b25852f1ed199e28f0f533e7a9de89b89821
yoshi-lyosha <atanov_wow@mail.ru>
2017-05-10 00:57:17 +0300

    Отредачил конфиг с местоположением бд

--- website/config.py
+++ website/config.py
@@ -5,6 +5,6 @@ SECRET_KEY = 'Glavnoe - eto funk v dushe, rebyata'
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'host_mount\\app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'host_mount/app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
