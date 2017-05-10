d2781f4ff0bdb613e7837f3c783cd033ebae9e06
eremeevfd <dartfedor@mail.ru>
2017-05-10 10:23:25 +0300

    new pages, little improvements for authorized/unathorized users

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('website.config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+lm = LoginManager(app)
 
 # lm = LoginManager()
 # lm.init_app(app)
