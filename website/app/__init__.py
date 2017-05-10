2d8785d735af5dd131cfa9b718eab9a28c302d53
yoshi-lyosha <atanov_wow@mail.ru>
2017-05-10 16:24:20 +0300

    Фикс под маунт в контейнер. Сменил местоположение бд и папки с миграциями. Оказывается, в VOLUME в докер-референсе нельзя прописывать host-dir. А жаль.

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -3,11 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+import os
+basedir = os.path.abspath(os.path.dirname(__file__) + '/../..')
+migration_dir = os.path.abspath(os.path.join(basedir, 'host_mount/migrations'))
+
 app = Flask(__name__)
 app.config.from_object('website.config')
 db = SQLAlchemy(app)
-migrate = Migrate(app, db, directory='website/host_mount/migrations')
+migrate = Migrate(app, db, directory=migration_dir)
 lm = LoginManager(app)
+
+print(basedir)
+print(migration_dir)
 
 # lm = LoginManager()
 # lm.init_app(app)
